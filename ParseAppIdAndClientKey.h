bbdb1f6e488240c1eee3d5d39dffb4bdbfbe9388
Grover Light <grover.light@gmail.com>
2015-09-22 18:28:54 -0700

    1st commit
    [+] updated parse
    [+] removed unwanted images

---
+++ ParseAppIdAndClientKey.h
@@ -0,0 +1,14 @@
+
+//! \file   ParseAppIdAndClientKey.h
+//! \brief  This file contains the Parse ApplicationId and ClientKey.
+//!
+//!
+//__________________________________________________________________________________________________
+
+#import <UIKit/UIKit.h>
+//__________________________________________________________________________________________________
+
+//style
+#define PARSE_APPLICATION_ID  @"yRbOL711EgHv0tq1HANe9cBOsOGVn2uwClXXdlZD"
+#define PARSE_CLIENT_KEY      @"LNFGibcieBKM0IWUbONJWz7YZHh6LOYX1K0k3VH8"
+//__________________________________________________________________________________________________
